863d1bb102e33a176660f8813994b9a292912c36
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2022-02-15 11:23:59 +0100

    Add ESLint import plugin to sort imports correctly

--- js/.eslintrc.js
+++ js/.eslintrc.js
@@ -1,7 +1,31 @@
 module.exports = {
-    extends: ["eslint-config-reforis", "prettier"],
-    plugins: ["prettier"],
+    extends: ["eslint-config-reforis", "prettier", "plugin:import/recommended"],
+    plugins: ["prettier", "import"],
     rules: {
+        "import/order": [
+            "error",
+            {
+                groups: [
+                    "builtin",
+                    "external",
+                    "internal",
+                    ["parent", "sibling"],
+                ],
+                pathGroups: [
+                    {
+                        pattern: "react",
+                        group: "external",
+                        position: "before",
+                    },
+                ],
+                pathGroupsExcludedImportTypes: ["react"],
+                "newlines-between": "always",
+                alphabetize: {
+                    order: "asc",
+                    caseInsensitive: true,
+                },
+            },
+        ],
         "prettier/prettier": ["error"],
     },
 };
