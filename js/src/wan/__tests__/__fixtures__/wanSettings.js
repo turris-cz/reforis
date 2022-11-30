374b87d9476f359366abe1dfa448f0eccd627829
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2022-11-30 16:08:42 +0100

    Add new & fix tests

--- js/src/wan/__tests__/__fixtures__/wanSettings.js
+++ js/src/wan/__tests__/__fixtures__/wanSettings.js
@@ -10,10 +10,6 @@ export function wanSettingsFixture() {
         interface_count: 1,
         interface_up_count: 1,
         last_seen_duid: "12345678d123d12345c4",
-        mac_settings: {
-            custom_mac_enabled: false,
-            mac_address: "00:00:00:00:00:00",
-        },
         proto: "dhcp",
         up: true,
         wan6_settings: {
@@ -22,6 +18,14 @@ export function wanSettingsFixture() {
         wan_settings: {
             wan_dhcp: {},
             wan_type: "dhcp",
+        },
+        mac_settings: {
+            custom_mac_enabled: false,
+            mac_address: "00:00:00:00:00:00",
+        },
+        vlan_settings: {
+            enabled: false,
+            vlan_id: 1,
         },
     };
 }
@@ -38,6 +42,28 @@ export function customMACFixture() {
         mac_settings: {
             custom_mac_enabled: true,
             custom_mac: "11:11:11:11:11:11",
+        },
+        vlan_settings: {
+            enabled: false,
+        },
+    };
+}
+
+export function customVLANFixture() {
+    return {
+        wan_settings: {
+            wan_dhcp: {},
+            wan_type: "dhcp",
+        },
+        wan6_settings: {
+            wan6_type: "none",
+        },
+        mac_settings: {
+            custom_mac_enabled: false,
+        },
+        vlan_settings: {
+            enabled: true,
+            vlan_id: 22,
         },
     };
 }
