e667def5e8df941463bbf3a061a1176f153c7269
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2024-04-10 16:57:55 +0200

    Refactor update_reboot_settings function in updater.py

--- reforis/foris_controller_api/modules/updater.py
+++ reforis/foris_controller_api/modules/updater.py
@@ -1,4 +1,4 @@
-#  Copyright (C) 2020 CZ.NIC z.s.p.o. (http://www.nic.cz/)
+#  Copyright (C) 2020-2024 CZ.NIC z.s.p.o. (https://www.nic.cz/)
 #
 #  This is free software, licensed under the GNU General Public License v3.
 #  See /LICENSE for more information.
@@ -250,8 +250,9 @@ views = [
 
 
 def update_reboot_settings(settings):
+    settings = {"reboots": settings}
     return current_app.backend.perform(
         "router_notifications",
-        "update_reboot_settings",
+        "update_settings",
         settings,
     )
