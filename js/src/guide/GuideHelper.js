f3a7d958ef9c2587a8c2c0e4026a52dc958560e4
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2024-09-02 15:54:00 +0200

    Refactor GuideHelper & GuideControls components

--- js/src/guide/GuideHelper.js
+++ js/src/guide/GuideHelper.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019-2022 CZ.NIC z.s.p.o. (https://www.nic.cz/)
+ * Copyright (C) 2019-2024 CZ.NIC z.s.p.o. (https://www.nic.cz/)
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See /LICENSE for more information.
@@ -43,20 +43,20 @@ export default function GuideHelper({
     }
 
     return (
-        <div className="card guide-card bg-info text-white mb-4">
-            <div className="card-body row">
-                <div className="col-lg-8 col-md-12 col-sm-12 d-flex flex-column justify-content-center align-items-sm-center text-center">
-                    {stepContent.initial && (
-                        <ParagraphsArray content={stepContent.initial} />
-                    )}
-                    {isCompletedVisible && (
-                        <p className="font-weight-bold mb-sm-3 mb-md-0">
-                            {stepContent.completed}
-                        </p>
-                    )}
-                </div>
-                <GuideControls ws={ws} next_step={next_step} />
+        <div className="d-flex flex-column flex-sm-row justify-content-center p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3 mb-4">
+            <div className="flex-fill d-flex flex-column justify-content-center align-items-sm-center text-center mb-2 mb-sm-0 me-0 me-sm-3">
+                {stepContent.initial && (
+                    <ParagraphsArray content={stepContent.initial} />
+                )}
+                {isCompletedVisible && (
+                    <p
+                        className={`fw-bold mb-sm-3 mb-md-0 ${stepContent.initial ? "mt-2" : ""}`.trim()}
+                    >
+                        {stepContent.completed}
+                    </p>
+                )}
             </div>
+            <GuideControls ws={ws} next_step={next_step} />
         </div>
     );
 }
@@ -71,10 +71,14 @@ ParagraphsArray.propTypes = {
 function ParagraphsArray({ content }) {
     return Array.isArray(content) ? (
         // eslint-disable-next-line react/no-array-index-key
-        content.map((line) => (
-            <p key={line.length} dangerouslySetInnerHTML={{ __html: line }} />
+        content.map((line, index) => (
+            <p
+                className={`mb-${index === content.length - 1 ? 2 : 0}`}
+                key={line.length}
+                dangerouslySetInnerHTML={{ __html: line }}
+            />
         ))
     ) : (
-        <p dangerouslySetInnerHTML={{ __html: content }} />
+        <p className="mb-0" dangerouslySetInnerHTML={{ __html: content }} />
     );
 }
