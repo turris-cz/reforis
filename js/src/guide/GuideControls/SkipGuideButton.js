bc9aa1a63be7fe8b366d4550870baf11a334ca3e
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2024-09-16 16:33:24 +0200

    Refactor SkipGuideButton to use a modal for skipping guide
    This commit refactors the SkipGuideButton component to use a modal
    for skipping the guide. It introduces a new SkipGuideModal component
    that displays a warning message and allows the user to confirm or
    cancel skipping the guide. The SkipGuideButton component now triggers
    the modal when clicked, and the guide is skipped only when the user
    confirms. This improves the user experience and provides a clear way
    to skip the guide.

--- js/src/guide/GuideControls/SkipGuideButton.js
+++ js/src/guide/GuideControls/SkipGuideButton.js
@@ -5,10 +5,10 @@
  * See /LICENSE for more information.
  */
 
-import React from "react";
+import React, { useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Button } from "foris";
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "foris";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
 
@@ -19,22 +19,78 @@ SkipGuideButton.propTypes = {
 };
 
 export function SkipGuideButton({ next_step }) {
+    const [clicked, setClicked] = useState(false);
+    const [modalShown, setModalShown] = useState(false);
+
     const onGuideFinishHandler = useGuideFinish();
     const disabled = next_step === "password";
+
+    const handleSkipGuide = (e) => {
+        setClicked(true);
+        onGuideFinishHandler(e);
+        setModalShown(false);
+    };
+
     return (
-        <Button
-            className={`btn-secondary me-2 align-self-stretch align-self-sm-center ${disabled ? "disabled" : ""}`.trim()}
-            style={{ pointerEvents: "not-allowed" }}
-            onClick={onGuideFinishHandler}
-            disabled={disabled}
-        >
-            <span className="d-flex flex-nowrap justify-content-center align-items-center align-self-stretch align-self-sm-center">
-                <span className="d-none d-sm-inline-block text-nowrap me-1">
-                    {_("Skip guide")}
+        <>
+            <SkipGuideModal
+                shown={modalShown}
+                setShown={setModalShown}
+                callback={handleSkipGuide}
+            />
+            <Button
+                className={`btn-secondary me-2 align-self-stretch align-self-sm-center ${disabled ? "disabled" : ""}`.trim()}
+                style={{ pointerEvents: "not-allowed" }}
+                onClick={() => setModalShown(true)}
+                disabled={disabled || clicked}
+                loading={clicked}
+            >
+                <span className="d-flex flex-nowrap justify-content-center align-items-center align-self-stretch align-self-sm-center">
+                    <span className="d-none d-sm-inline-block text-nowrap me-1">
+                        {_("Skip")}
+                    </span>
+                    <FontAwesomeIcon icon="fa-solid fa-forward" />
                 </span>
-                <FontAwesomeIcon icon="fa-solid fa-forward" />
-            </span>
-        </Button>
+            </Button>
+        </>
+    );
+}
+
+SkipGuideModal.propTypes = {
+    shown: PropTypes.bool.isRequired,
+    setShown: PropTypes.func.isRequired,
+    callback: PropTypes.func.isRequired,
+};
+
+function SkipGuideModal({ shown, setShown, callback }) {
+    return (
+        <Modal shown={shown} setShown={setShown}>
+            <ModalHeader setShown={setShown} title={_("Warning!")} />
+            <ModalBody>
+                <p>{_("Are you sure you want to skip the whole guide?")}</p>
+                <p>
+                    {_(
+                        "This will skip the guide and you will be redirected to the overview page."
+                    )}
+                </p>
+                <p>
+                    {_(
+                        "Settings that are not set up during the guide will be set to default values. And you will have to set them up manually."
+                    )}
+                </p>
+            </ModalBody>
+            <ModalFooter>
+                <Button
+                    className="btn-secondary"
+                    onClick={() => setShown(false)}
+                >
+                    {_("Cancel")}
+                </Button>
+                <Button className="btn-primary" onClick={callback}>
+                    {_("Confirm")}
+                </Button>
+            </ModalFooter>
+        </Modal>
     );
 }
 
