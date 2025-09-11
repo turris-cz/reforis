8b8b8d3764c9c04f74258e14db73b0b3324136bf
Stepan Henek <stepan@bigdigdata.com>
2025-09-11 13:38:23 +0200

    Remove flup (fastcgi) use WSGIServer (debug) or eventlet instead
    This will allow us to finally drop flup dependency.

--- reforis/__main__.py
+++ reforis/__main__.py
@@ -1,6 +1,6 @@
 #
 # reforis
-# Copyright (C) 2021 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
+# Copyright (C) 2021-2026 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,27 +17,70 @@
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 #
 
-from flup.server.fcgi import WSGIServer
 
+import logging
+import socket
+import pathlib
+
+import click
 from . import create_app
 
+logger = logging.getLogger(__name__)
 
-class AppWrapper:
-    def __init__(self, app):
+try:
+    import eventlet
+    from eventlet import wsgi
+
+    eventlet.monkey_patch()
+    logger.info("Eventlet server will be used")
+    use_eventlet = True
+except ImportError:
+    use_eventlet = False
+    logger.warn("Flask debug server is used")
+
+
+class ScriptNameMidleware:
+    def __init__(self, app, prefix=None):
         self.app = app
+        self.prefix = prefix
 
     def __call__(self, environ, start_response):
-        environ["SCRIPT_NAME"] = "/reforis"
-        return self.app(environ, start_response)
+        if environ["PATH_INFO"].startswith(self.prefix):
+            environ["PATH_INFO"] = environ["PATH_INFO"][len(self.prefix) :]
+            environ["SCRIPT_NAME"] = self.prefix
+            return self.app(environ, start_response)
+        else:
+            start_response("404", [("Content-Type", "text/plain")])
+            return ["This url does not belong to the app.".encode()]
 
 
-def main():
-    app = AppWrapper(create_app("prod"))
+@click.command()
+@click.option("--debug/--no-debug", default=False)
+@click.option("--port", default=None)  # port option for debugging purposes
+@click.option("--prefix", default="/reforis")
+@click.option("--unix-socket", default="/var/run/reforis.sock")
+def main(port, prefix, debug, unix_socket):
+    app = create_app("prod")
+    app.wsgi_app = ScriptNameMidleware(app.wsgi_app, prefix=prefix)
 
-    WSGIServer(
-        app,
-        debug=True,
-    ).run()
+    if port:
+        if use_eventlet:
+            wsgi.server(eventlet.listen(("127.0.0.1", port)), app)
+        else:
+            # Runs server in development mode
+            # this should be used in initial setup only
+            # the preffered way is to use eventlet
+            app.run(debug=debug, port=port, threaded=True)
+    else:
+        # Remove existing socket
+        pathlib.Path(unix_socket).unlink(missing_ok=True)
+        if use_eventlet:
+            wsgi.server(eventlet.listen(unix_socket, family=socket.AF_UNIX), app)
+        else:
+            # Runs server in development mode
+            # this should be used in initial setup only
+            # the preffered way is to use eventlet
+            app.run(debug=debug, host=f"unix://{unix_socket}", threaded=True)
 
 
 if __name__ == "__main__":
